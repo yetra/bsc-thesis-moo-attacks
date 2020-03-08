0756f257c3afcc0de6294f3e3549664444b28613
yetra <brdujmovic@gmail.com>
2020-03-08 11:41:58 +0100

    Add problem arg to Solution init

--- src/solution.py
+++ src/solution.py
@@ -9,10 +9,19 @@ class Solution:
         objectives: the objectives in a given decision variables vector
     """
 
-    def __init__(self):
-        """Initializes Solution attributes."""
+    def __init__(self, problem=None):
+        """Initializes Solution attributes.
+
+        If the problem argument is not None, the decision vector's values will be randomized.
+
+        Args:
+            problem: an object representing the MOOP to optimize
+        """
         self.variables = []
         self.objectives = []
+
+        if problem:
+            self._randomize(problem)
 
     def _randomize(self, problem):
         """Randomizes the decision space variables of this solution.
