11d2acc2f9ad20caf107ad18e6f0d467dc127af4
yetra <brdujmovic@gmail.com>
2020-03-08 12:03:06 +0100

    Refactor arithmetic crossover

--- src/crossover/arithmetic_crossover.py
+++ src/crossover/arithmetic_crossover.py
@@ -17,17 +17,10 @@ class ArithmeticCrossover(Crossover):
 
     def of(self, first_parent, second_parent):
         """Returns two child solutions obtained by arithmetically crossing the given parents."""
-        if len(first_parent.variables) != len(second_parent.variables):
-            raise ValueError("Parent variable vectors must be of the same length!")
+        first_child, second_child = Solution(), Solution()
 
-        first_child = Solution()
-        second_child = Solution()
-
-        for i in range(len(first_parent.variables)):
-            fist_alpha = self.alpha * first_parent.variables[i]
-            second_alpha = self.alpha * second_parent.variables[i]
-
-            first_child.variables.append(fist_alpha + second_parent.variables[i] - second_alpha)
-            second_child.variables.append(first_parent.variables[i] - fist_alpha + second_alpha)
+        for v1, v2 in zip(first_parent.variables, second_parent.variables):
+            first_child.variables.append(self.alpha * v1 + (1 - self.alpha) * v2)
+            second_child.variables.append((1 - self.alpha) * v1 + self.alpha * v2)
 
         return first_child, second_child
