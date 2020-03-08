9e439d4364dada1dcc4cd871f4cfb131dbf2fd0b
yetra <brdujmovic@gmail.com>
2020-03-08 11:32:51 +0100

    Add Problem class

---
+++ src/problem.py
@@ -0,0 +1,22 @@
+class Problem:
+    """Models a multi-objective optimization problem.
+
+    Attributes:
+        variables_count: the number of decision space variables that this problem requires
+        objectives_count: the number of objectives to optimize
+        mins: the lowest possible values of each decision space variable
+        maxs: the highest possible values of each decision space variable
+        objective_mins: the lowest possible values of each objective
+        objective_maxs: the highest possible values of each objective
+    """
+
+    def __init__(self, variables_count, objectives_count, mins, maxs, objective_mins, objective_maxs):
+        """Initializes Problem attributes."""
+        self.variables_count = variables_count
+        self.objectives_count = objectives_count
+
+        self.mins = mins
+        self.maxs = maxs
+
+        self.objective_mins = objective_mins
+        self.objective_maxs = objective_maxs
