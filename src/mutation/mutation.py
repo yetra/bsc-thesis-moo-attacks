94894c89ebd6a1877cd7fd03f9e817169adaea9a
yetra <brdujmovic@gmail.com>
2020-03-08 12:24:48 +0100

    Remove Mutation#of method

--- src/mutation/mutation.py
+++ src/mutation/mutation.py
@@ -4,13 +4,6 @@ from abc import ABC, abstractmethod
 class Mutation(ABC):
     """The base class to be extended by different mutation operators."""
 
-    def of(self, solution):
-        """Returns a mutated copy of the given solution."""
-        copied = solution.copy()
-        self.mutate(copied)
-
-        return copied
-
     @abstractmethod
     def mutate(self, solution):
         """Mutates the given solution."""
