8f5dd453c10064e11e50ae464586a0c6a7eedfff
yetra <brdujmovic@gmail.com>
2020-03-10 11:15:19 +0100

    Instantiate correct child class in ArithmeticCrossover

--- src/crossover/arithmetic_crossover.py
+++ src/crossover/arithmetic_crossover.py
@@ -17,7 +17,7 @@ class ArithmeticCrossover(Crossover):
 
     def of(self, first_parent, second_parent):
         """Returns two child solutions obtained by arithmetically crossing the given parents."""
-        first_child, second_child = Solution(), Solution()  # TODO instantiate appropriate class
+        first_child, second_child = first_parent.__class__(), second_parent.__class__()
 
         for v1, v2 in zip(first_parent.variables, second_parent.variables):
             first_child.variables.append(self.alpha * v1 + (1 - self.alpha) * v2)
