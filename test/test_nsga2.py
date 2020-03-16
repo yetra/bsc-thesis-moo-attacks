94447948a99513ff8a19520014a74f4ea6b49c13
yetra <brdujmovic@gmail.com>
2020-03-16 15:35:42 +0100

    Update NSGA2 test program

--- test/test_nsga2.py
+++ test/test_nsga2.py
@@ -2,7 +2,8 @@ from src.algorithms.nsga2 import NSGA2
 from src.crossover.arithmetic_crossover import ArithmeticCrossover
 from src.mutation.gaussian_mutation import GaussianMutation
 from src.selection.tournament_selection import TournamentSelection
-from test.test_problem1 import TestProblem1
+from test_problem1 import TestProblem1
+# from test_problem2 import TestProblem2
 
 if __name__ == '__main__':
     problem = TestProblem1()
@@ -11,8 +12,18 @@ if __name__ == '__main__':
     population_size = 100
     max_iterations = 1000
 
-    crossover = ArithmeticCrossover(0.5)
-    mutation = GaussianMutation(0.0, 1.0, 0.03)
+    crossover = ArithmeticCrossover(0.5, problem)
+    mutation = GaussianMutation(0.0, 1.0, 0.03, problem)
     selection = TournamentSelection(2)
 
-    NSGA2(problem, population_size, max_iterations, crossover, mutation, selection).run()
+    fronts = NSGA2(problem, population_size, max_iterations,
+                   crossover, mutation, selection).run()
+
+    for i, front in enumerate(fronts):
+        print(f'Front {i}: {len(front)} elements')
+
+        for solution in front:
+            print(f'{solution.objectives[0]}')
+        print('\n')
+        for solution in front:
+            print(f'{solution.objectives[1]}')
