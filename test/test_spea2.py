6062ae52382fb760995bff97ab3fa54e3b476238
yetra <brdujmovic@gmail.com>
2020-03-24 21:00:40 +0100

    Update SPEA2 parameters

--- test/test_spea2.py
+++ test/test_spea2.py
@@ -2,18 +2,18 @@ from algorithms.spea2 import SPEA2
 from src.crossover.arithmetic_crossover import ArithmeticCrossover
 from src.mutation.gaussian_mutation import GaussianMutation
 from src.selection.tournament_selection import TournamentSelection
-from test_problem1 import TestProblem1
-# from test_problem2 import TestProblem2
+# from test_problem1 import TestProblem1
+from test_problem2 import TestProblem2
 
 if __name__ == '__main__':
-    problem = TestProblem1()
-    # problem = TestProblem2()
+    # problem = TestProblem1()
+    problem = TestProblem2()
 
     population_size = 100
     archive_size = 100
-    max_iterations = 1000
+    max_iterations = 100
 
-    crossover = ArithmeticCrossover(0.5, problem)
+    crossover = ArithmeticCrossover(0.7, problem)
     mutation = GaussianMutation(0.0, 1.0, 0.03, problem)
     selection = TournamentSelection(2)
 
