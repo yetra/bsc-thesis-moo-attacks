580bad6ddf805cd137513a8127c70acf1a2d070c
yetra <brdujmovic@gmail.com>
2020-03-24 21:00:10 +0100

    Fix SPEA2 test program output

--- test/test_spea2.py
+++ test/test_spea2.py
@@ -17,13 +17,13 @@ if __name__ == '__main__':
     mutation = GaussianMutation(0.0, 1.0, 0.03, problem)
     selection = TournamentSelection(2)
 
-    fronts = SPEA2(problem, population_size, archive_size, max_iterations,
-                   crossover, mutation, selection).run()
+    archive = SPEA2(problem, population_size, archive_size, max_iterations,
+                    crossover, mutation, selection).run()
 
-    print(f'\nPrinting objectives for front 0 with {len(fronts[0])} elements')
+    print(f'\nPrinting objectives for archive with {len(archive)} elements')
 
     for i in range(problem.objectives_count):
         print(f'\nObjective {i}:')
 
-        for solution in fronts[0]:
+        for solution in archive:
             print(f'{solution.objectives[i]}')
