b7309378232f28e89e0ea12c474059c429ffd2d3
yetra <brdujmovic@gmail.com>
2020-04-08 13:36:12 +0200

    Refactor simple model

--- models/simple_model.py
+++ models/simple_model.py
@@ -1,47 +1,30 @@
-import keras
-from keras.datasets import mnist
 from keras.layers import Dense, Activation
 from keras.models import Sequential
-from matplotlib import pyplot as plt
 
-INPUT_SIZE = 28 * 28
-OUTPUT_SIZE = 10
+import util
 
-LAYER_SIZES = [128, OUTPUT_SIZE]
+INPUT_SHAPE = (28 * 28, )
+OUTPUTS = 10
+
+LAYER_SIZES = [128, 10]
 ACTIVATION_FUNCTIONS = ['relu', 'softmax']
 
 BATCH_SIZE = 128
 EPOCHS = 40
 
 
-def prepare_data(input_size, output_size):
-    """Prepares the MNIST dataset for training."""
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    x_train = x_train.reshape(x_train.shape[0], input_size).astype('float32')
-    x_test = x_test.reshape(x_test.shape[0], input_size).astype('float32')
-
-    x_train /= 255
-    x_test /= 255
-
-    y_train = keras.utils.to_categorical(y_train, output_size)
-    y_test = keras.utils.to_categorical(y_test, output_size)
-
-    return x_train, y_train, x_test, y_test
-
-
-def build_model(input_size, layer_sizes, activation_functions):
+def build_model(input_shape, layer_sizes, activation_functions):
     """
     Builds a simple, non-convolutional model.
 
-    :param input_size: the number of inputs
+    :param input_shape: a tuple describing the shape of the input layer
     :param layer_sizes: a list of hidden layer & output layer sizes
     :param activation_functions: a list of activation functions for each layer
     :return: the model created from the given parameters
     """
     model = Sequential()
 
-    model.add(Dense(layer_sizes[0], input_dim=input_size))
+    model.add(Dense(layer_sizes[0], input_shape=input_shape))
     model.add(Activation(activation_functions[0]))
 
     for i, size in enumerate(layer_sizes[1:]):
@@ -51,26 +34,11 @@ def build_model(input_size, layer_sizes, activation_functions):
     return model
 
 
-def plot_results(history):
-    """
-    Plots the model's accuracy over epochs on the training and validation sets.
-
-    :param history: a History object containing loss and metrics values
-    """
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['training', 'validation'], loc='best')
-    plt.show()
-
-
 def main():
     """Builds, trains and displays the results of a model."""
-    x_train, y_train, x_test, y_test = prepare_data(INPUT_SIZE, OUTPUT_SIZE)
+    x_train, y_train, x_test, y_test = util.load_mnist(INPUT_SHAPE, OUTPUTS)
 
-    model = build_model(INPUT_SIZE, LAYER_SIZES, ACTIVATION_FUNCTIONS)
+    model = build_model(INPUT_SHAPE, LAYER_SIZES, ACTIVATION_FUNCTIONS)
     model.summary()
 
     model.compile(loss='categorical_crossentropy',
@@ -79,7 +47,7 @@ def main():
     history = model.fit(x_train, y_train, epochs=EPOCHS,
                         batch_size=BATCH_SIZE, validation_split=.1)
 
-    plot_results(history)
+    util.plot_results(history)
 
     loss, accuracy = model.evaluate(x_test, y_test, verbose=False)
     print(f'\ntest loss: {loss:.3}, test accuracy: {accuracy:.3}')
