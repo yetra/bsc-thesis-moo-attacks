f5876426b8531578b4decd3178a8055f938cff63
yetra <brdujmovic@gmail.com>
2020-04-08 11:21:27 +0200

    Normalize MNIST data

--- models/simple_model.py
+++ models/simple_model.py
@@ -18,8 +18,11 @@ def prepare_data(input_size, output_size):
     """Prepares the MNIST dataset for training."""
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    x_train = x_train.reshape(x_train.shape[0], input_size)
-    x_test = x_test.reshape(x_test.shape[0], input_size)
+    x_train = x_train.reshape(x_train.shape[0], input_size).astype('float32')
+    x_test = x_test.reshape(x_test.shape[0], input_size).astype('float32')
+
+    x_train /= 255
+    x_test /= 255
 
     y_train = keras.utils.to_categorical(y_train, output_size)
     y_test = keras.utils.to_categorical(y_test, output_size)
