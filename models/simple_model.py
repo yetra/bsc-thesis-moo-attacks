a4a48753d6580acd6b45130c154de380faee8df2
yetra <brdujmovic@gmail.com>
2020-05-02 11:19:44 +0200

    Add SimpleModel train method

--- models/simple_model.py
+++ models/simple_model.py
@@ -51,6 +51,29 @@ class SimpleModel:
 
         return model
 
+    def train(self, data, epochs=40, batch_size=128):
+        """
+        Trains this model and saves the resulting weights.
+
+        :param data: a tuple of training data and test data with labels
+        :param epochs: the number of epochs
+        :param batch_size: the batch size
+        """
+        x_train, y_train, x_test, y_test = data
+
+        self.model.compile(loss=self.LOSS, optimizer=self.OPTIMIZER,
+                           metrics=['accuracy'])
+
+        history = self.model.fit(x_train, y_train, epochs=epochs,
+                                 batch_size=batch_size, validation_split=.1)
+
+        util.plot_results(history)
+
+        loss, accuracy = self.model.evaluate(x_test, y_test, verbose=False)
+        print(f'\ntest loss: {loss:.3}, test accuracy: {accuracy:.3}')
+
+        self.model.save_weights(f'simple_{time.strftime("%Y%m%d_%H%M%S")}.h5')
+
 
 def build_model(input_shape, layer_sizes, activation_functions):
     """
