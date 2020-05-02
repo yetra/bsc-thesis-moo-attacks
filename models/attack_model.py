914798c5ca6e812d52bb87095135cfd4801d58f8
yetra <brdujmovic@gmail.com>
2020-05-02 12:06:17 +0200

    Extract common model methods to AttackModel

---
+++ models/attack_model.py
@@ -0,0 +1,54 @@
+import time
+from abc import ABC, abstractmethod
+
+import util
+
+
+class AttackModel(ABC):
+    """The base class for attack models."""
+
+    def __init__(self, layer_sizes, activations):
+        """
+        Inits AttackModel attributes.
+
+        :param layer_sizes: a tuple of hidden layer & output layer sizes
+        :param activations: a tuple of activation functions for each layer
+        """
+        self.layer_sizes = layer_sizes
+        self.activations = activations
+
+        self.model = self._build()
+        self.model.summary()
+
+    @abstractmethod
+    def _build(self):
+        """Builds this model."""
+
+    def train(self, data, epochs, batch_size):
+        """
+        Trains this model.
+
+        :param data: a tuple of training and test data with labels
+        :param epochs: the number of epochs
+        :param batch_size: the batch size
+        """
+        x_train, y_train, x_test, y_test = data
+
+        self.model.compile(loss=self.LOSS, optimizer=self.OPTIMIZER,
+                           metrics=['accuracy'])
+
+        history = self.model.fit(x_train, y_train, epochs=epochs,
+                                 batch_size=batch_size, validation_split=.1)
+
+        util.plot_results(history)
+
+        loss, accuracy = self.model.evaluate(x_test, y_test, verbose=False)
+        print(f'\ntest loss: {loss:.3}, test accuracy: {accuracy:.3}')
+
+        self.model.save_weights(f'weights_{time.strftime("%Y%m%d_%H%M%S")}.h5')
+
+    def load(self, weights_path):
+        """Compiles this model using weights loaded from the specified path."""
+        self.model.load_weights(weights_path)
+        self.model.compile(loss=self.LOSS, optimizer=self.OPTIMIZER,
+                           metrics=['accuracy'])
