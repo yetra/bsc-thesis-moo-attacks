e5ba29a9c4bbd38527ebeff4254dc5f14b32ff47
yetra <brdujmovic@gmail.com>
2020-05-04 19:54:51 +0200

    Remove operator classes

--- moo/crossover/crossover.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Crossover(ABC):
-    """The base class to be extended by different crossover operators."""
-
-    @abstractmethod
-    def of(self, first_parent, second_parent):
-        """Returns two child solutions obtained by crossing the parents."""
--- moo/mutation/mutation.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Mutation(ABC):
-    """The base class to be extended by different mutation operators."""
-
-    @abstractmethod
-    def mutate(self, solution):
-        """Mutates the given solution."""
