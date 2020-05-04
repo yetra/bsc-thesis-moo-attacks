d5da91d363e783d93c118a44f4455105716ec1b3
yetra <brdujmovic@gmail.com>
2020-05-04 21:44:03 +0200

    Refactor test problems

--- test/test_problem1.py
+++ test/test_problem1.py
@@ -11,35 +11,18 @@ class TestProblem1(Problem):
     f3(x1, x2, x3, x4) = x3^2,
     f4(x1, x2, x3, x4) = x4^2
     """
+    NUM_VARIABLES = 4
+    NUM_OBJECTIVES = 4
+    MINS = [-5.0, -5.0, -5.0, -5.0]
+    MAXS = [5.0, 5.0, 5.0, 5.0]
 
     def __init__(self):
         """Initializes TestProblem1 attributes."""
-        super().__init__()
-
-        self.num_variables = 4
-        self.num_objectives = 4
-
-        self.mins = [-5.0] * self.num_variables
-        self.maxs = [5.0] * self.num_variables
-
-        self.o_mins = [None] * self.num_objectives
-        self.o_maxs = [None] * self.num_objectives
+        super().__init__(self.NUM_VARIABLES, self.NUM_OBJECTIVES,
+                         self.MINS, self.MAXS)
 
     def evaluate(self, population, orig_image, label):
         """Evaluates the given solution."""
         for solution in population:
-            objectives = []
-
-            for i, v in enumerate(solution.variables):
-                objective = v * v
-                objectives.append(objective)
-
-                if (self.o_mins[i] is None
-                        or objective < self.o_mins[i]):
-                    self.o_mins[i] = objective
-
-                elif (self.o_maxs[i] is None
-                      or objective > self.o_maxs[i]):
-                    self.o_maxs[i] = objective
-
-            solution.objectives = np.array(objectives)
+            solution.objectives = np.array([v * v for v in solution.variables])
+            self._update_o_extremes(solution)
--- test/test_problem2.py
+++ test/test_problem2.py
@@ -9,30 +9,19 @@ class TestProblem2(Problem):
     f1(x1, x2) = x1,
     f2(x1, x2) = (1 + x2) / x1
     """
+    NUM_VARIABLES = 2
+    NUM_OBJECTIVES = 2
+    MINS = [0.1, 0.0]
+    MAXS = [1.0, 5.0]
 
     def __init__(self):
         """Initializes TestProblem2 attributes."""
-        super().__init__()
-
-        self.num_variables = 2
-        self.num_objectives = 2
-
-        self.mins = [0.1, 0.0]
-        self.maxs = [1.0, 5.0]
-
-        self.o_mins = [None] * self.num_objectives
-        self.o_maxs = [None] * self.num_objectives
+        super().__init__(self.NUM_VARIABLES, self.NUM_OBJECTIVES,
+                         self.MINS, self.MAXS)
 
     def evaluate(self, population, orig_image, label):
         """Evaluates the given solution."""
         for solution in population:
-            solution.objectives = np.array(
-                [solution.variables[0],
-                 (1.0 + solution.variables[1]) / solution.variables[0]]
-            )
-
-            for i, o in enumerate(solution.objectives):
-                if self.o_mins[i] is None or o < self.o_mins[i]:
-                    self.o_mins[i] = o
-                elif self.o_maxs[i] is None or o > self.o_maxs[i]:
-                    self.o_maxs[i] = o
+            x1, x2 = solution.variables
+            solution.objectives = np.array([x1, (1.0 + x2) / x1])
+            self._update_o_extremes(solution)
