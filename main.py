2857de8858578ee03428dbeb4576ff06f9bd6af3
yetra <brdujmovic@gmail.com>
2020-05-05 20:47:12 +0200

    Add function for parsing command-line args

---
+++ main.py
@@ -0,0 +1,32 @@
+import argparse
+from matplotlib import pyplot as plt
+
+from convolutional_model import ConvolutionalModel
+from nsga2 import NSGA2
+from problem.simple_attack import SimpleAttack
+from problem.targeted_attack import TargetedAttack
+from simple_model import SimpleModel
+from spea2 import SPEA2
+
+
+def parse_args():
+    """Parses the command-line arguments."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('algorithm', choices=['nsga2', 'spea2'],
+                        help='the MOO algorithm to run')
+
+    parser.add_argument('attack_type', help='the type of attack to use',
+                        choices=['simple_attack', 'targeted_attack'])
+    parser.add_argument('noise_size', type=float, help='the size of the noise')
+
+    parser.add_argument('model', help='the model to attack',
+                        choices=['simple_model', 'convolutional_model'])
+    parser.add_argument('weights', help='path to the weights file')
+
+    parser.add_argument('--maxiter', type=int, default=100,
+                        help='the max number of algorithm iterations')
+    parser.add_argument('--popsize', type=int, default=100,
+                        help='the size of the population')
+
+    return parser.parse_args()
